2fb5a38f5f76060acd322a619593b3945ee4f933
ganfeng45 <1227525518@qq.com>
2020-05-17 21:23:10 +0800

    完成开发

--- app/src/main/java/com/example/time/timest.java
+++ app/src/main/java/com/example/time/timest.java
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.time.util.AccessibilityUtil;
+
 import java.lang.reflect.Method;
 
 
@@ -20,6 +22,10 @@ public class timest extends AppCompatActivity {
 
     private MenuItem mMenuItem;
     private androidx.fragment.app.FragmentManager fm = getSupportFragmentManager();
+    private AccessibilityUtil accessibilityUtil = null;
+    private static final String PACKAGE_NAME = "com.example.time";
+    private static final String SERVICE_NAME = "com.example.time.MyacService";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +36,20 @@ public class timest extends AppCompatActivity {
             Intent intent=new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
             startActivity(intent);
         }
+        accessibilityUtil = new AccessibilityUtil(timest.this,
+                PACKAGE_NAME, SERVICE_NAME);
+        openAccessibilityMet();
         startFragment(new UsageStatisticsFragment(), false);
     }
-
+    public void openAccessibilityMet()
+    {
+        //服务状态
+        boolean status = AccessibilityUtil.isAccessibilitySettingsOn(this, SERVICE_NAME);
+        if (!status)
+        {
+            accessibilityUtil.openSettingDialog();
+        }
+    }
 
     @Override
     protected void onDestroy() {
